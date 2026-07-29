9ad9f93ff88148da8b2ab33676778642c19cd6b9
viresh-v <viresh-v@hcltech.com>
2026-07-29 10:44:06 +0530

    Delete src directory

--- src/main/java/com/nms/eventprocessing/publisher/ValidatedEventPublisher.java
+++
@@ -1,8 +0,0 @@
-package com.nms.eventprocessing.publisher;
-
-import com.nms.eventprocessing.entity.DeviceEvent;
-
-public interface ValidatedEventPublisher {
-
-    void publish(DeviceEvent deviceEvent);
-}
--- src/main/java/com/nms/eventprocessing/repository/DeviceEventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nms.eventprocessing.repository;
-
-import com.nms.eventprocessing.entity.DeviceEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeviceEventRepository extends JpaRepository<DeviceEvent, Long> {
-}
